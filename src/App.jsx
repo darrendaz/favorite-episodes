786f4112215e7a2ae86d64fd160b986f9f51e904
darrendaz <darren.lou@gmail.com>
2019-09-14 15:59:03 -0700

    splitting code

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,32 @@
 import React from "react"
+
 import { Store } from "./Store"
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-import HomePage from "./HomePage"
-import FavPage from "./FavPage"
 
-export default function App() {
-  const { state, dispatch } = React.useContext(Store)
+export default function App(props) {
+  const state = React.useContext(Store)
 
-  const fetchDataAction = async () => {
-    const data = await fetch(
-      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes"
-    )
-    const dataJSON = await data.json()
-
-    return dispatch({
-      type: "FETCH_DATA",
-      payload: dataJSON._embedded.episodes
-    })
-  }
-
-  const toggleFavAction = episode => {
-    const episodeInFavorites = state.favorites.includes(episode)
-    let dispatchObj = {
-      type: "ADD_FAV",
-      payload: episode
-    }
-
-    if (episodeInFavorites) {
-      const favoritesWithoutEpisode = state.favorites.filter(
-        fav => fav.id !== episode.id
-      )
-      dispatchObj = {
-        type: "REMOVE_FAV",
-        payload: favoritesWithoutEpisode
-      }
-    }
-    return dispatch(dispatchObj)
-  }
-
-  React.useEffect(() => {
-    state.episodes.length === 0 && fetchDataAction()
-  })
-  const props = {
-    episodes: state.episodes,
-    toggleFavAction: toggleFavAction,
-    favorites: state.favorites
-  }
   return (
     <React.Fragment>
-      <React.Suspense fallback={<div>Loading...</div>}>
-        <div className="header">
-          <h1>Rick and Morty</h1>
-          <p>Pick your favourite episodes</p>
-        </div>
+      <div className="header">
+        <h1>Rick and Morty</h1>
+        <p>Pick your favourite episodes</p>
+      </div>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/favorites">Favorite(s) {state.favorites.length}</Link>
+            </li>
+          </ul>
+        </nav>
 
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/favorites">
-                    Favorite(s) {state.favourites.length}
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-
-            <Route path="/" exact component={HomePage} />
-            <Route path="/favorites" exact component={FavPage} />
-          </div>
-        </Router>
-        <section className="episode-layout">
-          <EpisodesList {...props} />
-        </section>
-      </React.Suspense>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/favorites" exact component={FavPage} />
+      </Router>
+      {props.children}
     </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import { StoreProvider } from "./Store"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import HomePage from "./HomePage"
+import FavPage from "./FavPage"
 import "./index.css"
 
 ReactDOM.render(
   <StoreProvider>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StoreProvider>,
   document.getElementById("root")
 )
